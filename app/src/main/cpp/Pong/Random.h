64854231ed87fa4ab2ebb8b95547995ab91e93b5
MartinSchubert04 <tinchoschubert04429@gmail.com>
2026-05-23 14:23:25 -0300

    fix props

--- app/src/main/cpp/Pong/Random.h
+++ app/src/main/cpp/Pong/Random.h
@@ -2,18 +2,16 @@
 
 #include "pch.h"
 
-inline int &getRandBool() {
+inline int getRandBool() {
   std::random_device rd;
   std::mt19937 gen(rd());
   std::uniform_int_distribution<int> dist(0, 1);
-  auto rand = dist(gen);
-  return rand;
+  return dist(gen);
 }
 
-inline float &getRandRange(float min, float max) {
+inline float getRandRange(float min, float max) {
   std::random_device rd;
   std::mt19937 gen(rd());
   std::uniform_real_distribution<float> dist(min, max);
-  auto rand = dist(gen);
-  return rand;
+  return dist(gen);
 }
